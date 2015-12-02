17a483338b024cca349712f20c9d82da86ee7558
Matt Reimer <mattdreimer@gmail.com>
2015-12-02 16:48:57 -0600

    Added CalibrateSpeed.py run this file to obtain a dictionary of rc8 values mapped to speeds between 0 and 8mph. It is a little cumbersome but you need to copy paste the dictionary between files.
    Modified Gcart.py to use dictionary of values obtained from CalibrateSpeed.py
    to set rc8.

--- CalibrateSpeed.py
+++ CalibrateSpeed.py
@@ -1,3 +1,8 @@
+#Run this file to obtain a dictionary for calibrating the speed in Gcart.py
+#After you follow the instructions on the screen and stop the tractor 
+#you will need to copy paste the dictionary into setSpeed function
+#setSpeed can be found at approx line 100 in Gcart.py
+
 from Tkinter import *
 import ttk
 from ttk import *
@@ -39,23 +44,6 @@ root.rowconfigure(0, weight=5)
 root.rowconfigure(5, weight=6)
 #~ root.rowconfigure(6, weight=2)
 
-###topInfoFrame###
-
-#Should have GpsStatus, Radio Link status and strength (see mavproxy console),
-#and Arm status of pixhawk (see mavproxy console)
-#leave some space for more information to be displayed
-
-###End of topInfo Frame####
-
-###tractorHealthFrame###
-
-#should have temp, oil, feul, and rpm of tractor displayed
-#should watch with a callback some mavlink message
-#mavlink message has not yet been implemented
-
-###End of tracHealthFrame###
-
-
 ###Terminal Frame###	
 text = Tkinter.Text(terminal)
 text.grid(column=0, row=0, sticky=(N,E,S,W))
@@ -96,19 +84,17 @@ def setTargetSpeed(scaleVal):
 def setSpeed(requestSpeed):
 	#Function for changing speed. requestSpeed is a number (0-whatever
 	#the top speed is set for) default is 8mph
-	#speedDict is a dictionary that can be obtained by running CalibrateSpeed.py
-	speedDict={0:1000, 1: 1100, 2: 1200, 3: 1500, 4: 1600, 5: 1700, 6: 1800, 7: 1900, 8: 2000}
 	speedScaleVal.set(requestSpeed)
 	setTargetSpeed(requestSpeed)
-	
-	if requestSpeed in speedDict:
-		rc8Val=speedDict[requestSpeed]
+	global rc8Val
+	if requestSpeed==0:
+		rc8Val=1000
 	else:
-		#map rc8 value if request speed is between two whole numbers
-		lowerVal= math.floor(requestSpeed)
-		upperVal= math.ceil(requestSpeed)
-		spread=speedDict[upperVal]-speedDict[lowerVal]
-		rc8Val=int((requestSpeed-lowerVal)*spread+speedDict[lowerVal])
+		rc8Val=int(requestSpeed/8.0*1000+1000)
+		if rc8Val>2000:
+			rc8Val=2000
+		elif rc8Val<1000:
+			rc8Val=1000
 	
 	v.channels.overrides = {8:rc8Val}
 
@@ -218,164 +204,73 @@ targetSpeedLabel.grid(column=2, row=0, sticky=(S,E,W))
 
 
 ###Start of Buttons###
-offsetAhead=18.0
-offsetLeft=9.5
-def distBwPoints(lat1,lon1,lat2,lon2):
-	#returns distance in meters between two points of lat and lon
-	#lat and lon need to be in decimal degrees
-	#going to use a,b,c triangle c is hyponteus
-	#theta is the distance in km of one degree of latitude
-	theta=111111.0
-	a=math.fabs(((lat1-lat2)*theta))
-	#~ print "a=", a
-	b=math.fabs((lon1-lon2))*math.cos(math.radians(math.fabs(lat1)))*theta
-	#~ print "b=",b
-	c=math.sqrt(a*a+b*b)
-	#~ print "c=",c
-	return c
 
-def getGpsLoc():
-	#returns list of lat,lon,track,speed
-	# Use the python gps package to access the laptop GPS
-	try:
-		gpsd = gps.gps(mode=gps.WATCH_ENABLE)
-		# Once we have a valid location (see gpsd documentation) we can start moving our vehicle around
-		# This is necessary to read the GPS state from the laptop
-		gpsd.next()
-		gotgps=True
-		while gotgps:
-			gpsd.next()
-			if (gpsd.valid & gps.LATLON_SET) != 0:
-				loc = [gpsd.fix.latitude, gpsd.fix.longitude, gpsd.fix.track,gpsd.fix.speed]
-				return loc
-	except socket.error:
-		print "Error the GPS does not seem to be Connected \n"
-	#uncomment below return statement to test program and comment out while loop above
-	#~ return [49.0,-99.0,270,0]
-
-def cartUnldLoc(distLeft,distAhead,combineLoc):
-	#returns lat lon that is dist left and dist ahead of combine location.
-	#if dist left or dist ahead is negative the offset will be behind
-	#combineLoc is a list with 4 elements in the same form that getGpsLoc returns
-	#angle between headingLeft and hyptoneus line formed by the triangle 
-	#created with distLeft + distAhead
-	theta=math.degrees(math.atan(float(distAhead)/float(distLeft)))
-	#alpha is the angle between 0(north) and the hyptoneus line 
-	#(if you drew a line between combineloc and projected point)
-	alpha=-1
-	if (combineLoc[2]-90+theta)<0:
-		alpha=math.radians(combineLoc[2]+270+theta)
-		#~ print "if statement"
-	else:
-		alpha=math.radians(combineLoc[2]-90+theta)
-		#~ print "else statement"
-	#delta lat and delta lon equals the sine and cosine of alpha multiplied 
-	#by hypotenues length (h)
-	h=math.sqrt(distLeft*distLeft+distAhead*distAhead)
-	deltaLat=math.cos(alpha)*h
-	deltaLon=math.sin(alpha)*h
-	#convert delta lat and delta lon to decimal degrees add to combine
-	#lat lon and return the result
-	deltaLat=deltaLat/111111.0
-	deltaLon=deltaLon/(math.cos(math.radians(combineLoc[0]))*111111.0)
-	lat=combineLoc[0]+deltaLat
-	lon=combineLoc[1]+deltaLon
-	loc=[lat,lon]
-	return loc
-
-nudge=0.0
-nudgeFront=0.0
-def setPointForward():
-	global nudgeFront
-	nudgeFront=15.0
-	print "Set point ahead ",nudgeFront,"(m) \n"
-
-def turnAround():
-	global nudge
-	print "CART IS TURNING AROUND!"
-	print "CART IS MOVING CLOSER! \n"
-	setSpeed(3.0)
-	nudge=0.0 #this should ensure cart always turns to the right
-	setPointForward()
-	
-sendCartControl = threading.Event()
+speedCalibrationControl = threading.Event()
+startLogging = threading.Event()
 sendCartStatus = False
 
-turnSet=False
-forwardSet=False
-
-def sendCart(sendCartControl):
+def speedCalibration(speedCalibrationControl):
 	#~ print "sendCartThreadisStarted"
-	global turnSet
-	global forwardSet
 	global sendCartStatus
+	global rc8Val
+	count=0
 	while True:
 		#~ print "sendCartThread is Running"
-		sendCartControl.wait()
-		print "Started sending tractor process"
+		speedCalibrationControl.wait()
+		count=count+1
+		if count==1:
+			print "Started Calibration process"
+		else:
+			print "Calibration done push stop button"
 		while v.mode.name!="GUIDED":
 			print "got here"
 			v.mode = VehicleMode("GUIDED")
 			time.sleep(1)
 			print v.mode.name
-		print "Tractor is in Gear \nStarting to get gps location of combine"
-		combineLoc=getGpsLoc()
-		print "Got Gps Location"
-		loc=cartUnldLoc(offsetLeft+nudge,offsetAhead+nudgeFront,combineLoc)
-		#check distance b/w cart and combine if distance is below some threshold execute turn cart around only do this once
-		if turnSet==False:
-			cartLoc=v.location.global_frame
-			distance=distBwPoints(loc[0],loc[1],cartLoc.lat,cartLoc.lon)
-			if 25.0>distance:
-				print "Turning Cart Around \n"
-				turnAround()
-				turnSet=True
-		if turnSet==True and forwardSet==False:
-			cartLoc=v.location.global_frame
-			distance=distBwPoints(combineLoc[0],combineLoc[1],cartLoc.lat,cartLoc.lon)
-			if 21.0>distance:
-				bringItClose()
-				forwardSet=True
-		#~ print "Distance = ", distance
-		loc=cartUnldLoc(offsetLeft+nudge,offsetAhead+nudgeFront,combineLoc)
-		cartGoalLoc=LocationGlobal(loc[0],loc[1],0)
-		v.simple_goto(cartGoalLoc)
-		#~ print "Sending Cart to ", cartLoc
+		print "Tractor is in Gear"
+		if count==1:
+			speedDict={0:1000}
+			for x in range(1,9):
+				print "Please adjust speed until tractor is going ", x," mph"
+				print "Please push log button and wait for more instructions"
+				startLogging.wait()
+				speedDict[x] = rc8Val
+				print speedDict
+				startLogging.clear()
+
+		time.sleep(1)
+			 
 		if sendCartStatus==False:
+			print "copy paste the following into blank file"
+			print speedDict
 			while v.mode.name!="HOLD":
 				v.mode = VehicleMode("HOLD")
-				time.sleep(1)		
-sendCartThread = threading.Thread(target=sendCart, args=(sendCartControl,))
-sendCartThread.start()
+				time.sleep(1)
+						
+speedCalibrationThread = threading.Thread(target=speedCalibration, args=(speedCalibrationControl,))
+speedCalibrationThread.start()
 
 def stop():
 	setSpeed(0)
-	global sendCartControl
+	global speedCalibrationControl
 	global sendCartStatus
-	sendCartControl.clear()
+	speedCalibrationControl.clear()
 	sendCartStatus=False
-	startUnloadingButton.grid()
 	v.mode = VehicleMode("HOLD")
 	
+def log():
+	global startLogging
+	startLogging.set()
+
 def print_mode():
 	pass
-		
-def startUnloading():
-	global nudge
-	nudge=15.0 #change this to set distance away from combine cart initially starts
-	global nudgeFront
-	nudgeFront=0.0
-	global turnSet
-	global forwardSet
-	global sendCartControl
+			
+def startCalibration():
+	global speedCalibrationControl
 	global sendCartStatus
 	sendCartStatus=True
-	turnSet=False
-	forwardSet=False
 	print v.mode.name
-	sendCartControl.set()
-	startUnloadingButton.grid_remove()
-	print "got there"
+	speedCalibrationControl.set()
 
 	
 for x in range(3):
@@ -397,11 +292,11 @@ buttonStyle.map("StartUnloading.Default.TButton",
 buttonStyle.configure("StartUnloading.Default.TButton",
 	background="green")	
 		
-armDisarmButton=ttk.Button(buttons, 
-	text="Arm", 
-	command= print_mode,
+logButton=ttk.Button(buttons, 
+	text="Log", 
+	command= log,
 	style="Default.TButton")
-armDisarmButton.grid(
+logButton.grid(
 	column=1, 
 	row=0, 
 	sticky=(N,E,S,W))	
@@ -416,8 +311,8 @@ stopButton.grid(
 	sticky=(N,E,S,W))
 
 startUnloadingButton=ttk.Button(buttons, 
-	text="Start\nUnloading", 
-	command= startUnloading,
+	text="Start\nCalibration", 
+	command= startCalibration,
 	style="StartUnloading.Default.TButton")
 startUnloadingButton.grid(
 	column=0, 
